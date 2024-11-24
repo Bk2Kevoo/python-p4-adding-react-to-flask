f69855857b94614faaafde5215d8d71404a8a306
+ <+>
2024-11-23 19:39:30 -0500

    done

--- server/app.py
+++ server/app.py
@@ -14,60 +14,31 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages', methods=['GET', 'POST'])
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    if request.method == 'GET':
-        messages = Message.query.order_by('created_at').all()
+    try:
+        if request.method == "GET":
+            #! how do I query all message OBJECTS
+            messages = Message.query
+            #! what can I return?
+            return make_response([message_obj.to_dict() for message_obj in messages], 200)
+        else:
+            #! 1. Retrieve data from the request body (JSON)
+            data = request.get_json()
+            #! 2. Instantiate a message object (use Message) and pass the info along
+            new_message = Message(body=data.get("body", "default text"), username=data.get("username"))
+            #! 3. Don't forget to add the new object to the session for tracking (and automatic SQL statement creation)
+            db.session.add(new_message)
+            #! 4. Commit the session to fire those SQL statements
+            db.session.commit()
+            #! return what you created!!!
+            return make_response(jsonify(new_message.to_dict()), 201)
+    except Exception as e:
+        return make_response({"error": str(e)}), 400
 
-        response = make_response(
-            jsonify([message.to_dict() for message in messages]),
-            200,
-        )
-    
-    elif request.method == 'POST':
-        data = request.get_json()
-        message = Message(
-            body=data['body'],
-            username=data['username']
-        )
-
-        db.session.add(message)
-        db.session.commit()
-
-        response = make_response(
-            jsonify(message.to_dict()),
-            201,
-        )
-
-    return response
-
-@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+@app.route('/messages/<int:id>')
 def messages_by_id(id):
-    message = Message.query.filter_by(id=id).first()
+    return ''
 
-    if request.method == 'PATCH':
-        data = request.get_json()
-        for attr in data:
-            setattr(message, attr, data[attr])
-            
-        db.session.add(message)
-        db.session.commit()
-
-        response = make_response(
-            jsonify(message.to_dict()),
-            200,
-        )
-
-    elif request.method == 'DELETE':
-        db.session.delete(message)
-        db.session.commit()
-
-        response = make_response(
-            jsonify({'deleted': True}),
-            200,
-        )
-
-    return response
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,10 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+
 
 class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String)
@@ -13,4 +21,4 @@ class Message(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
-        return f'<Message by {self.username}: {self.body[:10]}...>'
+        return f"Message #{self.id}: {self.username} says '{self.body}'. Created at: {self.created_at}"
